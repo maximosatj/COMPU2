94ad6d72263e83cf5497cadedb92acb96124aee4
Maximo Sat <127675329+maximosatj@users.noreply.github.com>
2023-09-27 18:47:56 -0300

    Remake fork (ej 4 y 5) [fork]
    Remake

--- 4 - 5 FORK/ej4.py
+++ 4 - 5 FORK/ej4.py
@@ -1,25 +1,19 @@
-#Escribir un programa que en ejecución genere dos procesos, uno padre y otro hijo.
-#El hijo debe escribir "Soy el hijo, PID YYYY" 5 veces (YYYY es el pid del hijo).
-#El padre debe escribir "Soy el padre, PID XXXX, mi hijo es YYYY" 2 veces (XXXX es el pid del padre).
-#El padre debe esperar a que termine el hijo y mostrar el mensaje "Mi proceso hijo, PID YYYY, terminó".
-#El hijo al terminar debe mostrar "PID YYYY terminando".
-
 import os
 import time
-import sys
 
 def child():
     for i in range(5):
-        print("Hi, im the child, PID", os.getpid())
+        print("Soy el hijo, PID", os.getpid())
         time.sleep(1)
-    print("PID", os.getpid(), "done")
+    print("PID", os.getpid(), "terminando")
 
 def parent():
+    global childpid
     for i in range(2):
-        print("Hi, im the parent, PID", os.getpid(), "my child is", childpid)
+        print("Soy el padre, PID", os.getpid(), "mi hijo es", childpid)
         time.sleep(1)
     os.wait()
-    print("My child process, PID", childpid, "has finished")
+    print("Mi proceso hijo, PID", childpid, "terminó")
 
 childpid = os.fork()
 if childpid == 0:
